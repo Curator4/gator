3598ea5661dd7cec8baf43d7505d232183f53280
curator <curator4@gmail.com>
2025-10-16 07:32:24 +0200

    sqlc queries

--- main.go
+++ main.go
@@ -5,12 +5,22 @@ import (
 	"fmt"
 	"log"
 	"errors"
+	"time"
+	"context"
+	"database/sql"
+	"github.com/google/uuid"
 	"github.com/curator4/gator/internal/config"
+	"github.com/curator4/gator/internal/database"
+	_ "github.com/lib/pq"
 )
+
+// constants
+const dbURL = "postgres://curator:solitude4@localhost:5432/gator?sslmode=disable"
 
 // structs
 type state struct {
 	cfg *config.Config
+	db *database.Queries
 }
 
 type command struct {
@@ -42,15 +52,23 @@ func main() {
 		log.Fatal(err)
 	}
 
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	s := &state{
 		cfg: &cfg,
+		db: database.New(db),
 	}
+
 	c := &commands{
 		handlers: make(map[string]func(*state, command) error),
 	}
 
 	c.register("login", handlerLogin)
-
+	c.register("register", handlerRegister)
+	
 	if len(os.Args) < 2 {
 		fmt.Printf("needs at least 2 arguments\n")
 		os.Exit(1)
@@ -64,7 +82,7 @@ func main() {
 	}
 
 	if err := c.run(s, cmd); err != nil {
-		fmt.Printf("could not run command?\n")
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 		os.Exit(1)
 	}
 }
@@ -76,9 +94,45 @@ func handlerLogin(s *state, cmd command) error {
 		return errors.New("login expects a single argument")
 	}
 	username := cmd.args[0]
+
+	if _, err := s.db.GetUser(context.Background(), username); err != nil {
+		return fmt.Errorf("user does not exist: %w", err)
+	}
+
 	if err := (*s.cfg).SetUser(username); err != nil {
 		return err
 	}
 	fmt.Printf("user: %s has been logged in\n", username)
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.args) != 1 {
+		return errors.New("register expects a single argument")
+	}
+	username := cmd.args[0]
+	if _, err := s.db.GetUser(context.Background(), username); err == nil {
+		return errors.New("user already exists")
+	}
+
+	current_time := time.Now()
+	params := database.CreateUserParams{
+		ID: uuid.New(),
+		CreatedAt: current_time,
+		UpdatedAt: current_time,
+		Name: username,
+	}
+
+	user, err := s.db.CreateUser(context.Background(), params)
+	if err != nil {
+		return fmt.Errorf("failed to create user: %w", err)
+	}
+	
+	if err := s.cfg.SetUser(username); err != nil {
+		return err
+	}
+
+	fmt.Printf("user %s was created\n", user.Name)
+	fmt.Printf("user data: %+v\n", user)
 	return nil
 }
